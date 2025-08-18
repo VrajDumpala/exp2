50d1742b98703c31775645f5d37a4c8b60b169a5
vraj-518 <dvraj_cse230518@mgit.ac.in>
2025-08-18 10:37:10 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,21 @@
+submitBtn.onclick = function (event) {
+    event.preventDefault();
+    validateForm();
+};
+
+function validateForm() {
+    const dob = document.getElementById('dob').value.trim();
+    const address = document.getElementById('address').value.trim();
+    const phno = document.getElementById('phno').value.trim();
+    const email = document.getElementById('email').value.trim();
+
+    const dobError = document.getElementById('doberror');
+    const addressError = document.getElementById('addresserror');
+    const phoneError = document.getElementById('phoneerror');
+    const emailError = document.getElementById('emailerror');
+
+    dobError.textContent = dob === "" ? "Please enter DOB" : "";
+    addressError.textContent = address === "" ? "Please enter address" : "";
+    phoneError.textContent = phno === "" ? "Please enter phone" : "";
+    emailError.textContent = email === "" ? "Please enter email" : "";
+}
